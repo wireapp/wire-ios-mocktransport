68a61e5b7351b5dc78426de2b3dcfd61d5424c6e
Vytis <vytis@proto.lt>
2017-05-23 09:48:27 +0200

    Push events for teams (#36)
    * Moved MockPushEvent to swift
    
    * Made a protocol that all mock push events will conform
    
    * Added update events for team
    
    * Added members events
    
    * Reverted the protocol
    
    * Removed parameter that is not used anymore
    
    * Added Danger
    
    * Using lowercase static methods
    
    * Updated copyright headers

--- Source/Model/MockEvent.m
+++ Source/Model/MockEvent.m
@@ -75,7 +75,12 @@ static ZMLogLevel_t const ZMLogLevel ZM_UNUSED = ZMLogLevelWarn;
           @[@(ZMTUpdateEventUserPushRemove),@"user.push-remove"],
           @[@(ZMTUpdateEventUserUpdate),@"user.update"],
           @[@(ZMTUPdateEventUserClientAdd),@"user.client-add"],
-          @[@(ZMTUpdateEventUserClientRemove),@"user.client-remove"]
+          @[@(ZMTUpdateEventUserClientRemove),@"user.client-remove"],
+          @[@(ZMTUpdateEventTeamCreate),@"team.create"],
+          @[@(ZMTUpdateEventTeamUpdate),@"team.update"],
+          @[@(ZMTUpdateEventTeamDelete),@"team.delete"],
+          @[@(ZMTUpdateEventTeamMemberJoin),@"team.member-join"],
+          @[@(ZMTUpdateEventTeamMemberLeave),@"team.member-leave"]
           ];
     });
     return mapping;
